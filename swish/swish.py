1da655b08520e8056e97f46a090e815ef1f3dea0
Nishant Rai <nishantrai18@rediff.com>
2019-09-30 15:53:03 -0700

    Add base optimization unit test for Swish module
    Add unit test using dummy optimization task to check whether Swish
    module learns correctly.

--- swish/swish.py
+++ swish/swish.py
@@ -1,10 +1,12 @@
 import torch
 import unittest
-
 import swish_cpp
+
+import torch.nn.functional as F
 
 from torch import nn
 from torch.autograd import gradcheck
+from tqdm import tqdm
 
 
 class SwishFunction(torch.autograd.Function):
@@ -56,6 +58,64 @@ class TestSwishGrad(unittest.TestCase):
                       torch.randn(20, dtype=torch.double, requires_grad=True))
             success = gradcheck(self.swish, inputs, eps=1e-6, atol=1e-4)
             assert success, "Failure for: {}".format(inputs)
+
+
+class TestBasicSwishTraining(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.device = torch.device("cpu")
+
+    @classmethod
+    def tearDownClass(self):
+        """
+        This code code is ran once after all tests.
+        """
+        pass
+
+    def target_func_1(self, x):
+        return x * F.sigmoid(x)
+
+    def target_func_2(self, x):
+        return x
+
+    def target_func_3(self, x):
+        return x * F.sigmoid(2.5 * x)
+
+    def run_training_for_instance(self, target_func):
+
+        num_epochs = 1000
+
+        inputs = torch.randn(20, requires_grad=False)
+
+        swish = Swish()
+        loss_fn = nn.SmoothL1Loss()
+        target = target_func(inputs) + (torch.randn_like(inputs) * 0.01)
+
+        optim = torch.optim.Adam(swish.parameters(), lr=1e-1)
+
+        tq = tqdm(range(num_epochs), desc="Training progress")
+
+        for t in tq:
+
+            y = swish(inputs)
+            loss = loss_fn(y, target)
+
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            tq.set_postfix({
+                "l1": loss.item(),
+                "grad:": swish.beta.grad.abs().mean()
+            })
+
+        print("Learned values:", list(swish.parameters()))
+
+    def test_basic_training(self):
+
+        self.run_training_for_instance(self.target_func_1)
+        self.run_training_for_instance(self.target_func_2)
+        self.run_training_for_instance(self.target_func_3)
 
 
 if __name__ == "__main__":
