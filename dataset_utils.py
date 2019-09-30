0547b166e4c4ca8f39fe6ba8e810cbb97d431d7b
Nishant Rai <nishantrai18@rediff.com>
2019-09-30 15:52:12 -0700

    Add support for CIFAR100 dataset
    This diff refactors the original baseline into more
    relevant components. Removes the existing deduplication
    and adds support for using CIFAR100. Other changes,
    - Add support for data augmentation
    - Preserve support for MNIST for quick experiments
    - Shift to tqdm for logging

---
+++ dataset_utils.py
@@ -0,0 +1,112 @@
+import torch
+
+from torchvision import datasets, transforms
+
+
+# Util helpers for dataset specific transformations
+###################################################
+
+def add_data_augmentation(transform_list):
+    # Add randomized cropping
+    transform_list.append(
+        transforms.RandomResizedCrop((32, 32), scale=(0.9, 1.0), ratio=(0.98, 1.02))
+    )
+    # Add random flipping
+    transform_list.append(transforms.RandomHorizontalFlip())
+    return transform_list
+
+
+def extend_base_mnist_transforms(transform_list):
+    transform_list.extend([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    return transform_list
+
+
+def extend_base_cifar100_transforms(transform_list):
+    transform_list.extend([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.507, 0.487, 0.441], std=[0.267, 0.256, 0.276])
+    ])
+    return transform_list
+
+
+# Data loader utilities
+#######################
+
+
+def fetch_mnist_dataloaders(args):
+
+    # Prepare the transforms
+    train_transform_list, test_transform_list = [], []
+
+    # Only add data augmentation to train transforms
+    if args.perform_data_aug:
+        train_transform_list = add_data_augmentation(train_transform_list)
+
+    # Add the base transformations
+    train_transform_list = extend_base_mnist_transforms(train_transform_list)
+    test_transform_list = extend_base_mnist_transforms(test_transform_list)
+
+    train_loader = \
+        torch.utils.data.DataLoader(
+            datasets.MNIST(
+                '../data',
+                train=True,
+                download=True,
+                transform=transforms.Compose(train_transform_list)
+            ),
+            batch_size=args.train_batch_size,
+            shuffle=True
+        )
+    test_loader = \
+        torch.utils.data.DataLoader(
+            datasets.MNIST(
+                '../data',
+                train=False,
+                transform=transforms.Compose(test_transform_list)
+            ),
+            batch_size=args.test_batch_size,
+            shuffle=True
+        )
+
+    return train_loader, test_loader
+
+
+def fetch_cifar100_dataloaders(args):
+
+    # Prepare the transforms
+    train_transform_list, test_transform_list = [], []
+
+    # Only add data augmentation to train transforms
+    if args.perform_data_aug:
+        train_transform_list = add_data_augmentation(train_transform_list)
+
+    # Add the base transformations
+    train_transform_list = extend_base_cifar100_transforms(train_transform_list)
+    test_transform_list = extend_base_cifar100_transforms(test_transform_list)
+
+    train_loader = \
+        torch.utils.data.DataLoader(
+            datasets.CIFAR100(
+                '../data',
+                train=True,
+                download=True,
+                transform=transforms.Compose(train_transform_list)
+            ),
+            batch_size=args.train_batch_size,
+            shuffle=True
+        )
+    test_loader = \
+        torch.utils.data.DataLoader(
+            datasets.CIFAR100(
+                '../data',
+                train=False,
+                transform=transforms.Compose(test_transform_list)
+            ),
+            batch_size=args.test_batch_size,
+            shuffle=True
+        )
+
+    return train_loader, test_loader
--- main.py
+++ main.py
@@ -1,43 +1,56 @@
 from __future__ import print_function
+
 import argparse
+import dataset_utils
 import torch
+
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
+
+from tqdm import tqdm
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, in_ch, num_classes):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv1 = nn.Conv2d(in_ch, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.fc1 = nn.Linear(5*5*50, 500)
+        self.fc2 = nn.Linear(500, num_classes)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
+        x = x.view(-1, 5*5*50)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
-    
+
+
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    tq = tqdm(train_loader, desc="Steps within train epoch {}:".format(epoch))
+    for batch_idx, (data, target) in enumerate(tq):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+        tq.set_postfix({"loss": loss.item()})
+
 
 def test(args, model, device, test_loader):
     model.eval()
@@ -47,8 +60,10 @@ def test(args, model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
@@ -57,60 +72,60 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser = argparse.ArgumentParser(description='SVL coding task')
+    parser.add_argument('--dataset', type=str, default="cifar100",
+                        help='Specifies the dataset (mnist, cifar100 - default)')
+    parser.add_argument('--train-batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
+    parser.add_argument('--perform-data-aug', type=str2bool, default=False,
+                        help='whether to perform data augmentation')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                         help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-    
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
+
     args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
 
-    device = torch.device("cuda" if use_cuda else "cpu")
+    # Only support CPU as a device
+    device = torch.device("cpu")
 
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    # Set the relevant information based on the dataset
+    if args.dataset == "mnist":
+        in_ch, num_classes = 1, 10
+        dataset_loader = dataset_utils.fetch_mnist_dataloaders
+    elif args.dataset == "cifar100":
+        in_ch, num_classes = 3, 100
+        dataset_loader = dataset_utils.fetch_cifar100_dataloaders
+    else:
+        assert False, "Incorrect dataset {} provided".format(args.dataset)
 
+    train_loader, test_loader = dataset_loader(args)
 
-    model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    model = Net(in_ch, num_classes).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
 
-    if (args.save_model):
-        torch.save(model.state_dict(),"mnist_cnn.pt")
-        
+    if args.save_model:
+        torch.save(model.state_dict(), args.dataset + "_cnn.pt")
+
+
 if __name__ == '__main__':
     main()
